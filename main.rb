4ecada64d0f647957f51e33ca1103e39879da172
smileyuichi <smile.yuichi.1013@gmail.com>
2020-10-08 14:11:51 +0900

    Deckクラス基礎を定義

--- main.rb
+++ main.rb
@@ -22,10 +22,38 @@ class Card
     end
 end
 
+class Deck
+    def initialize
+        #デッキの生成
+        @cards = []
 
+        mark = ["スペード","クラブ","ダイヤ","ハート"]
+        number = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+
+        #マークと数字の組み合わせを1つずつ生成して@cardsに格納していく
+        mark.each do |mark|
+            number.each do |number|
+                # markとnumberの組み合わせを一つずつcardクラスに渡し、それぞれのcardオブジェクトを生成する
+                card = Card.new(mark, number)
+                # 生成したcardを配列に格納していく
+                @cards << card
+            end
+        end
+        @cards.shuffle!
+    end
+
+    #カードを引く
+    def draw
+        #配列の先頭を抜き取る
+        @cards.shift
+    end
+    #デバッグ用
+    # def numberOfSheets
+    #     p @cards.size
+    # end
+end
 
 class Blackjack
-    p card.show
     def start
         money = Money.new
         puts <<~"text"
